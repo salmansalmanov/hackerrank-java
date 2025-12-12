10d24054db387add766595d5660f5d8e9935779d
Salman <salmanov2904@gmail.com>
2025-12-12 20:41:01 +0400

    Problem 2 Java Stdin And Stdout I

---
+++ src/easy/Problem2JavaStdinAndStdout1.java
@@ -0,0 +1,15 @@
+package easy;
+
+import java.util.Scanner;
+
+public class Problem2JavaStdinAndStdout1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
