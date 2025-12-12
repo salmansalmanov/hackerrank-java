e8af9099b9296bc41edd73dc671082776a6765bf
Salman <salmanov2904@gmail.com>
2025-12-12 21:08:20 +0400

    Problem 11 Java Int To String

---
+++ src/easy/Problem11JavaIntToString.java
@@ -0,0 +1,13 @@
+package easy;
+
+import java.util.Scanner;
+
+public class Problem11JavaIntToString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        String s = String.valueOf(n);
+        System.out.println("Good job");
+    }
+}
