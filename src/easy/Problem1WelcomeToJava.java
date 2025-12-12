7e40466d7991c2fc8748f5dbb257a0631568d0ce
Salman <salmanov2904@gmail.com>
2025-12-12 20:39:36 +0400

    Problem 1 Welcome To Java

---
+++ src/easy/Problem1WelcomeToJava.java
@@ -0,0 +1,8 @@
+package easy;
+
+public class Problem1WelcomeToJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
